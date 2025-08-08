a9184db8a238710e5f2433e593c9e056753340ae
alireza <alireza818800@gmail.com>
2025-08-08 17:25:23 +0330

    init

---
+++ shop/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import redirect, render,reverse
+
+
+
+def index(request):
+    return redirect(reverse ("shop:store"))
+    #return render(request, 'index.html')
+
+def store(request):
+    return render(request, 'store.html') 
+   
+def checkout(request):
+    return render(request, 'checkout.html')
+ 
+def product(request):       
+    return render(request, 'product.html') 
